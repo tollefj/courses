36a04309a339ebd6fd7fe519412151c0572277a5
Tollef Jørgensen <tollef@adtube.no>
2021-02-22 20:20:27 +0100

    logo + text

--- storybook/src/App.tsx
+++ storybook/src/App.tsx
@@ -19,36 +19,36 @@ function App() {
   return (
     <>
       <Header userName={SampleUser} menuItems={SampleMenu} />
-        <div id='content' style={{
-          backgroundImage: `url(${bgImg})`,
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover'
-        }}>
-          <h1>DEMO</h1>
-          <h2>demo of wide</h2>
-          <Select label='wide select' size='wide'>
-            this is a child of wide select
-            this is a child of wide select
-            this is a child of wide select
-            this is a child of wide select
-            this is a child of wide select
-            this is a child of wide select
-            this is a child of wide select
-            this is a child of wide select
-            this is a child of wide select
-            this is a child of wide select
-          </Select>
-          <h2>demo of narrow</h2>
-          <Select label='narrow select' size='narrow'>
-            this is a child of narrow select
-            this is a child of narrow select
-            this is a child of narrow select
-          </Select>
-          <h2>buttons</h2>
-          <Button label='testbtn' mode='primary' size='large' />
-          <br/>
-          <Button label='testbtn' mode='secondary' size='medium' />
-        </div>
+      <div id='content' style={{
+        backgroundImage: `url(${bgImg})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover'
+      }}>
+        <h1>DEMO</h1>
+        <h2>demo of wide</h2>
+        <Select label='wide select' size='wide'>
+          this is a child of wide select
+          this is a child of wide select
+          this is a child of wide select
+          this is a child of wide select
+          this is a child of wide select
+          this is a child of wide select
+          this is a child of wide select
+          this is a child of wide select
+          this is a child of wide select
+          this is a child of wide select
+        </Select>
+        <h2>demo of narrow</h2>
+        <Select label='narrow select' size='narrow'>
+          this is a child of narrow select
+          this is a child of narrow select
+          this is a child of narrow select
+        </Select>
+        <h2>buttons</h2>
+        <Button label='testbtn' mode='primary' size='large' />
+        <br/>
+        <Button label='testbtn' mode='secondary' size='medium' />
+      </div>
     </>
   );
 }
--- storybook/src/components/header/Header.tsx
+++ storybook/src/components/header/Header.tsx
@@ -1,8 +1,12 @@
 import './header.css';
+import { ReactComponent as SvvLogo } from '../../assets/SVG/svvlogo.svg';
+import clsx from 'clsx';
+import createClass from '../../utils/createClass';
 
 export interface HeaderProps {
     userName: { name: string, initials: string }
     menuItems: { field: string, url: string}[]
+    slim?: boolean
 }
 
 const ID = 'delutb-header';
@@ -12,11 +16,18 @@ export const Header = ({
     menuItems = [
         { field: 'Map', url: '/map' },
         { field: 'Data', url: '/data' }
-    ]
+    ],
+    slim = false
 }) => {
     return (
-        <div className={ID}>
-            ok m8
+        <div className={clsx(ID, slim && createClass(ID, 'slim'))}>
+            <div id='logo'>
+                <SvvLogo fill='whitesmoke' width={100} height={'80%'} />
+                <div id='logo-text'>
+                    <h2>Statens Vegvesen</h2>
+                    <p>Delingstjeneste for måle- og modelldata</p>
+                </div>
+            </div>
         </div>
     )
 }
